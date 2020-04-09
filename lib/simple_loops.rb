a07529086087ebc0e20de1a150491ae869a75625
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-09 00:53:06 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -25,3 +25,13 @@ def output_array(array)
     counter += 1
   end
 end
+
+def return_string_array (array)
+  counter = 0
+  new_array = []
+while counter < array.length do
+ new_array.push(array[counter].to_s)
+   counter += 1
+ end
+ new_array
+ebd
